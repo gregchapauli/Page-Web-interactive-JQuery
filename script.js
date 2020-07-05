e814e69303df6127bb0b772aa4ec78838e0d0d07
Gregory Caloin <gregchapauli@gmail.com>
2020-07-05 15:57:01 +0200

    Test Animations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    
-    $("#p1").click(function(){
-        $("#p5").toggle();
+$(function(){
+
+    $("#start").click(function(){
+        $("div").slideUp(5000).slideDown(3000).fadeOut(3000).fadeIn(3000);
     });
 
 });
